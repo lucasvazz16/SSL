bc6e293963acc7ff9fdb2392f7a8430ee632b53c
Lucas Manuel Vazquez <129413637+lucasvazz16@users.noreply.github.com>
2023-04-12 11:17:19 -0300

    Update hello.c

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+void crearArchivo()
+{
+    FILE *archivo;
+    archivo = fopen("output.txt","w");
+    fprintf(archivo,"Hello World!");
+    fclose(archivo);
+}
+int main(void)
+{
+    crearArchivo();
+   
+    return 0;
+}
